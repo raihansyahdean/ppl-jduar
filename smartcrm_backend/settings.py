a6d6513de0f8b8e23eb592e4397e2cd5b8640013
rafiftaris <rafif@alterra.id>
2020-02-07 22:37:05 +0700

    [CHORES] remove staticfile settings from settings.py

--- smartcrm_backend/settings.py
+++ smartcrm_backend/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'corsheaders',
 ]
 
@@ -114,12 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
 
 # TODO : SEBELUM DEPLOY GANTI VAR INI JADI ppl-smartcrm.herokuapp.com
 CSRF_TRUSTED_ORIGINS = ['localhost:8080']
