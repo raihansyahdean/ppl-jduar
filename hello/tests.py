89161dfd74c351e8a080480d0972738349017e3f
rafiftaris <rafif@alterra.id>
2020-02-08 00:38:20 +0700

    [RED] fix test assert

--- hello/tests.py
+++ hello/tests.py
@@ -1,8 +1,14 @@
+import json
 from django.test import TestCase, Client
+
+hello_body = {
+    "status_code": 200,
+    "message": "Hello World!"
+}
 
 # Create your tests here.
 class HelloTest(TestCase):
     def test_hello_world(self):
         response = Client().get("/hello/")
         self.assertEqual(response.status_code,200)
-        self.assertEqual(response.message,"Hello World!")
+        self.assertJSONEqual(str(response.content, encoding="utf8"),hello_body)
