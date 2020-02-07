7accd1b2c91dcb40accc796ea1b5830db5cdf8e7
rafiftaris <rafif@alterra.id>
2020-02-07 23:19:35 +0700

    [RED] hello world

--- hello/tests.py
+++ hello/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase, Client
 
 # Create your tests here.
-def test_hello_world(self):
-    response = Client().get("/hello/")
-    self.assertEqual(response.status_code,200)
-    self.assertEqual(response.message,"Hello World!")
+class HelloTest(TestCase):
+    def test_hello_world(self):
+        response = Client().get("/hello/")
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.message,"Hello World!")
