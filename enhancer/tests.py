985115cb344231abdab8316acb40d64e24817faa
Raihansyah Attallah Andrian <raihansyah.dean@gmail.com>
2020-02-24 14:15:40 +0700

    [RED] create test for compression function

--- enhancer/tests.py
+++ enhancer/tests.py
@@ -1,8 +1,20 @@
 from django.test import TestCase
 from .compressor import *
+from io import BytesIO
+from PIL import Image
+import sys
 
-# Create your tests here.
+test_img_dir = "images/original.jpg"
+
 class EnhancerTest(TestCase):
-    def test_compressor(self):
-        new_image = compress("original.jpg")
+    def test_compress_returns_smaller_image(self):
+        # Read the image to take original
+        img = Image.open(test_img_dir)
+        img_io = BytesIO()
+        img.save(img_io, format = 'JPEG')
+        ori_size = sys.getsizeof(img_io)
+
+        # Compress the same image
+        new_image = compress(test_img_dir)
         print(new_image)
+        
