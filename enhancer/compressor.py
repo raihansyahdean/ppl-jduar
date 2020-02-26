7b5618f3eabe8e845a5b4cb59f0d4f51e2cc4dfd
Raihansyah Attallah Andrian <raihansyah.dean@gmail.com>
2020-02-26 22:01:32 +0700

    [CHORES] added docstrings and fix ci for pylint

--- enhancer/compressor.py
+++ enhancer/compressor.py
@@ -1,18 +1,22 @@
-import sys
+"""
+Main module for compression.
+"""
 from io import BytesIO
 from PIL import Image
 from django.core.files import File
 
-# Function to compress an image from its directory
 def compress(image_file_dir):
+    """
+    Function to compress an image from its directory.
+    """
     try:
         img = Image.open(image_file_dir)
     except FileNotFoundError:
         ret_msg = "The file " + image_file_dir + " does not exist."
         return ret_msg
-    
+
     img_io = BytesIO()
-    img.save(img_io, format = 'JPEG', optimize = True)
-    new_image = File(img_io, name = image_file_dir.split("/")[-1])
+    img.save(img_io, format='JPEG', optimize=True)
+    new_image = File(img_io, name=image_file_dir.split("/")[-1])
 
     return new_image
--- enhancer/tests.py
+++ enhancer/tests.py
@@ -1,26 +1,37 @@
-from django.test import TestCase
-from .compressor import compress
-from io import BytesIO
-from PIL import Image
+"""
+Main module for tests in enhancer app.
+"""
 import sys
+from io import BytesIO
+from django.test import TestCase
+from PIL import Image
+from .compressor import compress
 
 class EnhancerTest(TestCase):
+    """
+    Main enhancer test class.
+    """
     def test_compress_returns_smaller_image(self):
-        # Read the image to take original image
+        """
+        Test to read an image from a directory and compare
+        sizes after compression.
+        """
         test_img_dir = "images/original.jpg"
         img = Image.open(test_img_dir)
         img_io = BytesIO()
-        img.save(img_io, format = 'JPEG')
+        img.save(img_io, format='JPEG')
         ori_size = sys.getsizeof(img_io)
 
-        # Compress the same image
         new_image = compress(test_img_dir)
         new_size = sys.getsizeof(new_image)
 
         self.assertTrue(ori_size > new_size)
 
     def test_compress_image_does_not_exist(self):
+        """
+        Test when image does not exists return error message.
+        """
         test_img_dir = "images/original2.jpg"
         message = compress(test_img_dir)
-        self.assertEquals(message, "The file images/original2.jpg does not exist.")
+        self.assertEqual(message, "The file images/original2.jpg does not exist.")
         
