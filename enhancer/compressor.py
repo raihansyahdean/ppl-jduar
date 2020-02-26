d2d3b1dfde5dae5fca7db13a8a70f7e052163ed3
Raihansyah Attallah Andrian <raihansyah.dean@gmail.com>
2020-02-26 15:32:55 +0700

    [GREEN] implement error handling when compress image does not exist

--- enhancer/compressor.py
+++ enhancer/compressor.py
@@ -5,7 +5,12 @@ from django.core.files import File
 
 # Function to compress an image from its directory
 def compress(image_file_dir):
-    img = Image.open(image_file_dir)
+    try:
+        img = Image.open(image_file_dir)
+    except FileNotFoundError:
+        ret_msg = "The file " + image_file_dir + " does not exist."
+        return ret_msg
+    
     img_io = BytesIO()
     img.save(img_io, format = 'JPEG', optimize = True)
     new_image = File(img_io, name = image_file_dir.split("/")[-1])
