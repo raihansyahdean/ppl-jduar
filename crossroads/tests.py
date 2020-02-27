a9582ef4cd4da39f214840aef22d5788990dda5f
rafiftaris <rafif@alterra.id>
2020-02-27 11:54:44 +0700

    [RED] communicate backend with dummy api

--- crossroads/tests.py
+++ crossroads/tests.py
@@ -2,33 +2,33 @@ import json
 from django.test import TestCase, Client
 
 payload = {
-    "registerPhoto":[
-    {
-      "position" : "front",
-      "image" : "<image>"
-    },
-    {
-      "position" : "right",
-      "image" : "<image>"
-    },
-    {
-      "position" : "left",
-      "image" : "<image>"
-    },
-    {
-      "position" : "bottom",
-      "image" : "<image>"
-    },
-    {
-      "position" : "top",
-      "image" : "<image>"
-    }
-  ]
+    "registerPhoto": [
+        {
+            "position": "front",
+            "image": "<image>"
+        },
+        {
+            "position": "right",
+            "image": "<image>"
+        },
+        {
+            "position": "left",
+            "image": "<image>"
+        },
+        {
+            "position": "bottom",
+            "image": "<image>"
+        },
+        {
+            "position": "top",
+            "image": "<image>"
+        }
+    ]
 }
+
 
 # Create your tests here.
 class CrossroadTest(TestCase):
     def test_hello_world(self):
         response = Client().post("/crossroads/send/", data=payload)
-        # self.assertEqual(response.status_code,200)
-        self.assertEqual(True,True)
+        self.assertEqual(response.status_code, 200)
