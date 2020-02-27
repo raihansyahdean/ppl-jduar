7c0a606d30dbcc3c6d4475af6d6036cc43653edc
rafiftaris <rafif@alterra.id>
2020-02-27 11:57:05 +0700

    [GREEN] communicate backend with dummy api

--- crossroads/views.py
+++ crossroads/views.py
@@ -4,5 +4,35 @@ import django.middleware.csrf
 import requests
 import json
 
+payload_example = {
+    "registerPhoto": [
+        {
+            "position": "front",
+            "image": "<image>"
+        },
+        {
+            "position": "right",
+            "image": "<image>"
+        },
+        {
+            "position": "left",
+            "image": "<image>"
+        },
+        {
+            "position": "bottom",
+            "image": "<image>"
+        },
+        {
+            "position": "top",
+            "image": "<image>"
+        }
+    ]
+}
+
+
 def send_photos_to_dummy(request):
-  return
+    csrf_token = django.middleware.csrf.get_token(request)
+    response = requests.post('http://dummy-smartcrm.herokuapp.com/payload/photos/', data=json.dumps(payload_example),
+                             headers={"CSRF-Token": csrf_token})
+    print(response)
+    return HttpResponse(response)
