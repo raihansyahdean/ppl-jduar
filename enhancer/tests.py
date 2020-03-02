18749c41756c86325fb3ced3a7f67ded1b4590ad
Raihansyah Attallah Andrian <raihansyah.dean@gmail.com>
2020-03-02 23:44:33 +0700

    [RED] add tests for converting image to data and data to image

--- enhancer/tests.py
+++ enhancer/tests.py
@@ -1,12 +1,13 @@
 """
 Main module for tests in enhancer app.
 """
+import base64
 import sys
 from io import BytesIO
 from django.test import TestCase
 from PIL import Image
 from .compressor import compress
-from .image_processor import delete_image
+from .image_processor import delete_image, data_to_image, image_to_data
 
 class EnhancerTest(TestCase):
     """
@@ -38,6 +39,41 @@ class EnhancerTest(TestCase):
         test_img_dir = "images/original2.jpg"
         message = compress(test_img_dir)
         self.assertEqual(message, "The file images/original2.jpg does not exist.")
+
+    def test_convert_image_to_data_success(self):
+        """
+        Test to convert an image to base64.
+        """
+        test_img_dir = "compressed_images/compressed_large.jpg"
+        result = image_to_data(test_img_dir)
+
+        image = Image.open(test_img_dir)
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        self.assertEqual(result, base64.b64encode(buffered.getvalue()))
+
+    def test_convert_image_to_data_file_not_found(self):
+        """
+        Test to convert an image fail when file not found.
+        """
+        test_img_dir = "compressed_images/compressed_large2.jpg"
+        result = image_to_data(test_img_dir)
+        self.assertEqual(result, "File compressed_images/compressed_large2.jpg not found.")
+
+    def test_convert_data_to_image_success(self):
+        """
+        Test to convert a base64 data to an image.
+        """
+        test_img_dir = "compressed_images/compressed_large.jpg"
+        image = Image.open(test_img_dir)
+
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue())
+
+        ret_msg = data_to_image(img_str, "decoded_image.jpg")
+        self.assertEqual(ret_msg, "Successful convert to images/decoded_image.jpg")
+        delete_image("images/decoded_image.jpg")
 
     def test_delete_image_successful(self):
         """
