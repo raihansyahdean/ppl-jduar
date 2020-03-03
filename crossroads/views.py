c3d059d197d0f84d2051f7bbb9e567137736dbf0
rafiftaris <rafif@alterra.id>
2020-03-03 11:34:01 +0700

    [CHORES] move payload_isvalid to new file

--- crossroads/views.py
+++ crossroads/views.py
@@ -6,32 +6,7 @@ from django.http import JsonResponse
 import django.middleware.csrf
 import requests
 
-IMAGE_TEMPLATE = "<image>"
-
-PAYLOAD_TEMPLATE = {
-    "data": [
-        {
-            "position": "front",
-            "image": IMAGE_TEMPLATE
-        },
-        {
-            "position": "right",
-            "image": IMAGE_TEMPLATE
-        },
-        {
-            "position": "left",
-            "image": IMAGE_TEMPLATE
-        },
-        {
-            "position": "bottom",
-            "image": IMAGE_TEMPLATE
-        },
-        {
-            "position": "top",
-            "image": IMAGE_TEMPLATE
-        }
-    ]
-}
+import crossroads.validator as validator
 
 INVALID_PAYLOAD_RESPONSE = {"status_code": 400, "message": "Invalid Payload"}
 
@@ -43,7 +18,7 @@ def send_photos_to_dummy(request):
     """
     body_unicode = request.body.decode('utf8')
     request_payload = json.loads(body_unicode)
-    if not payload_isvalid(request_payload):
+    if not validator.payload_isvalid(request_payload):
         return JsonResponse(json.loads(json.dumps(INVALID_PAYLOAD_RESPONSE)), status=400)
 
     csrf_token = django.middleware.csrf.get_token(request)
@@ -55,43 +30,3 @@ def send_photos_to_dummy(request):
     reformatted_response = response.content.decode('utf8').replace("\\", "")
     reformatted_response = reformatted_response[1:-1]  # Remove unnecessary '
     return JsonResponse(json.loads(reformatted_response))
-
-
-def payload_isvalid(payload):
-    """
-    Validate payload based on template.
-    :param payload: dictionary that will be dumped to json and sent as payload
-    :return: True if valid. False otherwise
-    """
-    valid_position_flag = {
-        "front": False,
-        "right": False,
-        "left": False,
-        "bottom": False,
-        "top": False}
-    try:
-        data = payload["data"]
-
-        # Check json keys
-        for photos in data:
-            position = photos["position"]
-            # Check for duplicate direction
-            if valid_position_flag[position]:
-                print("invalid flag")
-                return False
-            valid_position_flag[position] = True
-            image = photos["image"]
-            if not isinstance(image, str):
-                print("invalid type")
-                return False
-
-        # Check if there's missing images
-        for value in valid_position_flag.values():
-            if not value:
-                print("Missing direction")
-                return False
-
-        return True
-
-    except KeyError:
-        return False
