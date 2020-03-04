f4f0c300baef3916cbf60509d81631261dfe313c
rafiftaris <rafif@alterra.id>
2020-03-04 23:39:02 +0700

    [CHORES] define staging and prod env

--- smartcrm_backend/settings/production.py
+++ smartcrm_backend/settings/production.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from pathlib import Path
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = load_dotenv(dotenv_path=Path('..'))
+SECRET_KEY = 'q4tt+k=es^)rqb+90utglb6=^h1svoiqfr#5d*e26lb0sp$au3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
