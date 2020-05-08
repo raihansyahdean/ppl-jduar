dfa026677d7823e91cb54c52da6db721952f6465
rafiftaris <rafif@alterra.id>
2020-05-08 09:26:20 +0700

    [CHORES] add musl-dev

--- smartcrm_backend/settings.py
+++ smartcrm_backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,4 +143,3 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
-django_heroku.settings(locals())
