b4ab938f5e881b22dac39e87bb7d4795e446d60d
rafiftaris <rafif@alterra.id>
2020-05-27 17:42:01 +0700

    [CHORES] add docker registry

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@
 ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ec2-user/app
   export $(cat .env.staging | xargs)
+  docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD $DOCKERHUB_REGISTRY
+  docker pull $IMAGE:web
+  docker pull $IMAGE:nginx
   docker-compose up -d
 ENDSSH
