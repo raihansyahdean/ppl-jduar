c9b3eff5bd88aa2b19ff8a6788a1c5539cea15f9
rafiftaris <rafif@alterra.id>
2020-05-27 18:42:44 +0700

    [CHORES] fix image naming

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ec2-user/app
   export $(cat .env.staging | xargs)
   docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD $DOCKERHUB_REGISTRY
-  docker pull $IMAGE:web
-  docker pull $IMAGE:nginx
+  docker pull $WEB_IMAGE
+  docker pull $NGINX_IMAGE
   docker-compose up -d
 ENDSSH
