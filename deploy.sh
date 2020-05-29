c978f94329aff708f8c1472d94011a599f3c3cf6
rafiftaris <rafif@alterra.id>
2020-05-29 12:07:56 +0700

    [CHORES] fix docker pull

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ec2-user/smartcrm_backend
   export $(cat .env.staging | xargs)
   docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD $DOCKERHUB_REGISTRY
-  docker pull $DOCKERHUB_REGISTRY/$WEB_IMAGE
-  docker pull $DOCKERHUB_REGISTRY/$NGINX_IMAGE
+  docker pull $DOCKERHUB_USER/$WEB_IMAGE
+  docker pull $DOCKERHUB_USER/$NGINX_IMAGE
   docker-compose up -d
 ENDSSH
