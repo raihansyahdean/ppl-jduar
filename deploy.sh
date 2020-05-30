f8599053852c71df34fbcaed651ba5f2f596cd9a
rafiftaris <rafif@alterra.id>
2020-05-30 07:31:38 +0700

    [CHORES] copy all content to ec2

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,5 @@ ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
   cd /home/ec2-user/smartcrm_backend
   export $(cat .env.staging | xargs)
   docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD $DOCKERHUB_REGISTRY
-  docker pull $DOCKERHUB_USER/$WEB_IMAGE
-  docker pull $DOCKERHUB_USER/$NGINX_IMAGE
-  docker-compose -f docker-compose.aws.yml up -d
+  docker-compose up -d
 ENDSSH
