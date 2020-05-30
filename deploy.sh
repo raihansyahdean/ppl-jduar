5087eb8fbb908638dfa2232996125ab3f96026e3
rafiftaris <rafif@alterra.id>
2020-05-30 08:24:55 +0700

    [CHORES] fix scp directory

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
-  cd /home/ec2-user/smartcrm_backend
+  cd /home/ec2-user/jduar-nvidia-smart-crm
   export $(cat .env.staging | xargs)
   docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD $DOCKERHUB_REGISTRY
   docker-compose up -d
