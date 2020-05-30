9b6093f4ef2ad3827b65d4f4e3a30fd5d62cb781
rafiftaris <rafif@alterra.id>
2020-05-30 10:00:58 +0700

    [CHORES] fix nginx config

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS << 'ENDSSH'
-  cd /home/ec2-user/jduar-nvidia-smart-crm
+  cd /home/ec2-user/smartcrm_backend
   export $(cat .env.staging | xargs)
   docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD $DOCKERHUB_REGISTRY
-  docker-compose up -d
+  docker pull $DOCKERHUB_USER/$WEB_IMAGE
+  docker pull $DOCKERHUB_USER/$NGINX_IMAGE
+  docker-compose -f docker-compose.aws.yml up -d
 ENDSSH
